a7bd67496b60ab6d82b83abb407f8302c30c7863
mojdeh <mojdeh.rastgoo@gmail.com>
2017-02-03 12:34:52 +0100

    cleaning the directory S-2

--- catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/lemaitre/catkin_ws/src/pleora_polarcam/devel/include".split(';') if "/home/lemaitre/catkin_ws/src/pleora_polarcam/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "pleora_polarcam"
-PROJECT_SPACE_DIR = "/home/lemaitre/catkin_ws/src/pleora_polarcam/devel"
-PROJECT_VERSION = "1.0.0"
