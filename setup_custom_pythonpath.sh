83e402910e323d0109d5b1a25e3681e41adee24c
mojdeh <mojdeh.rastgoo@gmail.com>
2017-02-03 12:26:50 +0100

    cleaning S-1

--- setup_custom_pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/lemaitre/catkin_ws/src/pleora_polarcam/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
